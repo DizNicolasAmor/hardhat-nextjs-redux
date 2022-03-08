54439816e8f3d4e32773f10b8c93229906d271d9
Nicolas Diz <diz.nicolasamor@gmail.com>
2022-03-08 16:22:29 -0300

    MASTER: update NavbarCustom.tsx in FRONTEND

--- frontend/src/components/NavbarCustom.tsx
+++ frontend/src/components/NavbarCustom.tsx
@@ -1,23 +1,34 @@
 import React, { FC } from 'react';
 import { Button, Nav, Navbar, Container } from 'react-bootstrap';
+import { useSelector } from 'react-redux';
+import { getUser } from '../redux/slices/userSlice';
 
-const NavbarCustom: FC = () => (
-  <Navbar bg="light" expand="lg">
-    <Container>
-      <Navbar.Brand href="/">My ERC-20 dApp</Navbar.Brand>
-      <Navbar.Toggle aria-controls="navbarScroll" />
-      <Navbar.Collapse id="navbarScroll">
-        <Nav
-          className="me-auto my-2 my-lg-0"
-          style={{ maxHeight: '100px' }}
-          navbarScroll
-        >
-          <Nav.Link href="network">Network info</Nav.Link>
-          <Nav.Link href="token">Token info</Nav.Link>
-        </Nav>
-        <Button variant="outline-primary">Connect</Button>
-      </Navbar.Collapse>
-    </Container>
-  </Navbar>
-);
+const NavbarCustom: FC = () => {
+  const { address } = useSelector(getUser);
+
+  return (
+    <Navbar bg="light" expand="lg">
+      <Container>
+        <Navbar.Brand href="/">My ERC-20 dApp</Navbar.Brand>
+        <Navbar.Toggle aria-controls="navbarScroll" />
+        <Navbar.Collapse id="navbarScroll">
+          <Nav
+            className="me-auto my-2 my-lg-0"
+            style={{ maxHeight: '100px' }}
+            navbarScroll
+          >
+            <Nav.Link href="network">Network info</Nav.Link>
+            <Nav.Link href="token">Token info</Nav.Link>
+          </Nav>
+          {!address ? (
+            <Button variant="primary">Connect</Button>
+          ) : (
+            <Button variant="outline-primary">Disconnect</Button>
+          )}
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
+};
+
 export default NavbarCustom;
