0bb93f6704d359341b648a5f96ee1833b979604b
Nicolas Diz <diz.nicolasamor@gmail.com>
2022-03-12 09:55:02 -0300

    MASTER: update network page in FRONTEND

--- frontend/src/pages/network.tsx
+++ frontend/src/pages/network.tsx
@@ -9,16 +9,23 @@ const NetworkSection: React.FC = () => {
   const { address, balance } = useSelector(getUser);
   const chainIdText = chainId === 0 ? '' : String(chainId);
 
+  const renderContent = () => (
+    <div className="m-3">
+      {renderSectionItem('Chain id', chainIdText)}
+      {renderSectionItem('Network name', name)}
+      {renderSectionItem('Balance', `${balance} ${symbol}`)}
+      {renderSectionItem('Address', address)}
+    </div>
+  );
+
+  const renderDefaultMessage = () => (
+    <p className="m-3">You have to connect your wallet to see this section.</p>
+  );
+
   return (
     <section aria-labelledby="network-section" className="text-center">
       <h1 id="network-section">Welcome to the Network section</h1>
-
-      <div className="m-3">
-        {renderSectionItem('Chain id', chainIdText)}
-        {renderSectionItem('Network name', name)}
-        {renderSectionItem('Balance', `${balance} ${symbol}`)}
-        {renderSectionItem('Address', address)}
-      </div>
+      {chainId ? renderContent() : renderDefaultMessage()}
     </section>
   );
 };
